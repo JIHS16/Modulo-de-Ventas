b9d8df9552b28ba6e6fa716e7dcefbad80b46f21
JIHS16 <114714049+JIHS16@users.noreply.github.com>
2022-11-07 11:06:28 -0500

    Add files via upload

---
+++ ModuloVentas/src/main/java/com/G3ModuloVentas/ModuloVentas/ModuloVentasApplication.java
@@ -0,0 +1,13 @@
+package com.G3ModuloVentas.ModuloVentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloVentasApplication.class, args);
+	}
+
+}
---
+++ ModuloVentas/src/test/java/com/G3ModuloVentas/ModuloVentas/ModuloVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G3ModuloVentas.ModuloVentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
